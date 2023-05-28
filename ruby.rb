66ede161c41e81a9c37431d516bca17ef8e675e1
Chris <123729087+chrish121@users.noreply.github.com>
2023-05-28 23:08:03 +0800

    Edit code to follow the Style Guide

--- ruby.rb
+++ ruby.rb
@@ -1,67 +1,54 @@
-class Player  
+# frozen_string_literal: true
+
+# This class contains details about the players.
+class Player
   def self.start(player_name)
     puts "#{player_name}, what is your name?"
     @name = gets.chomp.strip
     puts "#{player_name}, what do you want to use? Enter only 1 letter or symbol."
     preferred_symbol = gets.chomp.strip
-    if preferred_symbol.length != 1 || preferred_symbol == Player_One.symbol || preferred_symbol.match?(/[[:digit:]]/) == true
-      until preferred_symbol.length == 1 && preferred_symbol != Player_One.symbol && preferred_symbol.match?(/[[:digit:]]/) == false
-        if !Player_One.symbol
-          puts "Invalid. Try again. Enter only 1 letter or symbol."
-          preferred_symbol = gets.chomp.strip
-        else
-          puts "Invalid. Try again. Enter only 1 letter or symbol. #{Player_One.symbol} has been taken by #{Player_One.name}."
-          preferred_symbol = gets.chomp.strip
-        end
+    check_symbol(preferred_symbol)
+    @symbol = @new_symbol.to_s
+  end
+
+  def self.check_symbol(symbol_input)
+    until symbol_input.length == 1 && symbol_input != PlayerOne.symbol &&
+          symbol_input.match?(/[[:digit:]]/) == false
+      if !PlayerOne.symbol
+        puts 'Invalid. Try again. Enter only 1 letter or symbol.'
+      else
+        puts "Invalid. Try again. #{PlayerOne.symbol} has been taken by #{PlayerOne.name}."
       end
+      symbol_input = gets.chomp.strip
     end
-    @symbol = preferred_symbol
+    @new_symbol = symbol_input
   end
 
-  def self.name()
-    return @name
+  class << self
+    attr_reader :name
   end
 
-  def self.symbol()
-    return @symbol
+  class << self
+    attr_reader :symbol
   end
 end
 
-class Player_One < Player
+class PlayerOne < Player
 end
 
-class Player_Two < Player
+class PlayerTwo < Player
 end
 
+# This class contains methods for the tic-tac-toe board.
 class Board
-  @winner = "none"
+  @winner = 'none'
 
   def self.first_turn
-    puts "Who plays first? #{Player_One.name}, #{Player_Two.name}, or random?"
-    first_turn_answer = gets.chomp.strip
-    until first_turn_answer.downcase == Player_One.name.downcase || 
-      first_turn_answer.downcase == Player_Two.name.downcase || 
-      first_turn_answer.downcase == "random"
-      puts "Invalid. Try again."
-      first_turn_answer = gets.chomp.strip
-    end
-    if first_turn_answer.downcase == Player_One.name.downcase
-      @first_turn_player = Player_One
-      @first_turn_player_name = Player_One.name
-    elsif first_turn_answer.downcase == Player_Two.name.downcase
-      @first_turn_player = Player_Two
-      @first_turn_player_name = Player_Two.name
-    elsif first_turn_answer.downcase == "random"
-      random_number = Random.new.rand(1..10)
-      if random_number.odd?
-        @first_turn_player = Player_One
-        @first_turn_player_name = Player_One.name
-      else
-        @first_turn_player = Player_Two
-        @first_turn_player_name = Player_Two.name
-      end
-    end
-    puts "#{@first_turn_player_name} plays first."
+    puts "Who plays first? #{PlayerOne.name}, #{PlayerTwo.name}, or random?"
+    @first_turn_answer = gets.chomp.strip
+    check_first_turn_validity
+    pick_first_player
+    puts "#{@first_turn_player.name} plays first."
   end
 
   def self.show_board
@@ -69,73 +56,92 @@ class Board
     puts @current_board
   end
 
+  def self.game
+    ask_for_move(@first_turn_player)
+    change_board(@first_turn_player)
+    which_player_next until @winner == 'yes' || @winner == 'neither'
+  end
+
   def self.ask_for_move(which_player)
     puts "#{which_player.name}, pick a number on the tic-tac-toe board for your turn."
     @move = gets.chomp.strip.to_i
-    until (@move > 0 && @move < 10) && (@current_board.include?(@move.to_s))
-      puts "Invalid. Pick another number for your turn"
+    until (@move.positive? && @move < 10) && @current_board.include?(@move.to_s)
+      puts 'Invalid. Pick another number for your turn.'
       @move = gets.chomp.strip.to_i
     end
   end
 
   def self.change_board(which_player)
-    @new_board = @current_board.sub("#{@move.to_s}", "#{(which_player).symbol}")
+    @new_board = @current_board.sub(@move.to_s, which_player.symbol.to_s)
     puts @new_board
     @current_board = @new_board
   end
 
-  def self.check_for_winner(which_player_one, which_player_two)
+  def self.check_for_results(which_player_one, which_player_two)
     players = [which_player_one, which_player_two]
     players.each do |player|
-      if player.symbol == @current_board[2] && player.symbol == @current_board[8] && player.symbol == @current_board[14] ||
-        player.symbol == @current_board[37] && player.symbol == @current_board[43] && player.symbol == @current_board[49] ||
-        player.symbol == @current_board[72] && player.symbol == @current_board[78] && player.symbol == @current_board[84] ||
-        player.symbol == @current_board[2] && player.symbol == @current_board[37] && player.symbol == @current_board[72] ||
-        player.symbol == @current_board[8] && player.symbol == @current_board[43] && player.symbol == @current_board[78] ||
-        player.symbol == @current_board[14] && player.symbol == @current_board[49] && player.symbol == @current_board[84] ||
-        player.symbol == @current_board[2] && player.symbol == @current_board[43] && player.symbol == @current_board[84] ||
-        player.symbol == @current_board[72] && player.symbol == @current_board[43] && player.symbol == @current_board[14]
-        puts "#{player.name} is the winner!"
-        @winner = "yes"
-        return "#{player.name} is the winner!"
-      elsif @winner == "none" && @current_board.match?(/[[:digit:]]/) == false
-        puts "It's a draw!"
-        @winner = "neither"
-        return "It's a draw!"
-      end
+      @player = player
+      @a = player.symbol
+      check_for_winner
+      next unless @winner == 'none' && @current_board.match?(/[[:digit:]]/) == false
+
+      puts "It's a draw!"
+      @winner = 'neither'
+      return "It's a draw!"
     end
   end
 
-
-  def self.game
-    Board.ask_for_move(@first_turn_player)
-    Board.change_board(@first_turn_player)
-    until @winner == "yes" || @winner == "neither"
-      if @first_turn_player == Player_One
-        Board.ask_for_move(Player_Two)
-        Board.change_board(Player_Two)
-        Board.check_for_winner(Player_One, Player_Two)
-        if @winner == "none"
-          Board.ask_for_move(Player_One)
-          Board.change_board(Player_One)
-          Board.check_for_winner(Player_One, Player_Two)
-        end
-      elsif @first_turn_player == Player_Two
-        Board.ask_for_move(Player_One)
-        Board.change_board(Player_One)
-        Board.check_for_winner(Player_One, Player_Two)
-        if @winner == "none"
-          Board.ask_for_move(Player_Two)
-          Board.change_board(Player_Two)
-          Board.check_for_winner(Player_One, Player_Two)
-        end
-      end
+  def self.check_for_winner
+    @b = @current_board
+    if @a == @b[2] && @a == @b[8] && @a == @b[14] || @a == @b[37] && @a == @b[43] && @a == @b[49] ||
+       @a == @b[72] && @a == @b[78] && @a == @b[84] || @a == @b[2] && @a == @b[37] && @a == @b[72] ||
+       @a == @b[8] && @a == @b[43] && @a == @b[78] || @a == @b[14] && @a == @b[49] && @a == @b[84] ||
+       @a == @b[2] && @a == @b[43] && @a == @b[84] || @a == @b[72] && @a == @b[43] && @a == @b[14]
+      puts "#{@player.name} is the winner!"
+      @winner = 'yes'
+      "#{@player.name} is the winner!"
     end
+  end
+
+  def self.check_first_turn_validity
+    until @first_turn_answer.to_s.downcase == PlayerOne.name.downcase ||
+          @first_turn_answer.to_s.downcase == PlayerTwo.name.downcase ||
+          @first_turn_answer.to_s.downcase == 'random'
+      puts 'Invalid. Try again.'
+      @first_turn_answer = gets.chomp.strip
+    end
+  end
+
+  def self.pick_first_player
+    if @first_turn_answer.to_s.downcase == PlayerOne.name.downcase
+      @first_turn_player = PlayerOne
+    elsif @first_turn_answer.to_s.downcase == PlayerTwo.name.downcase
+      @first_turn_player = PlayerTwo
+    elsif @first_turn_answer.to_s.downcase == 'random'
+      random_number = Random.new.rand(1..10)
+      @first_turn_player = random_number.odd? ? PlayerOne : PlayerTwo
+    end
+  end
+
+  def self.which_player_next
+    if @first_turn_player == PlayerOne
+      player_turn(PlayerTwo)
+      player_turn(PlayerOne) if @winner == 'none'
+    elsif @first_turn_player == PlayerTwo
+      player_turn(PlayerOne)
+      player_turn(PlayerTwo) if @winner == 'none'
+    end
+  end
+
+  def self.player_turn(other_player)
+    Board.ask_for_move(other_player)
+    Board.change_board(other_player)
+    Board.check_for_results(PlayerOne, PlayerTwo)
   end
 end
 
 Board.show_board
-Player_One.start("Player 1")
-Player_Two.start("Player 2")
+PlayerOne.start('Player 1')
+PlayerTwo.start('Player 2')
 Board.first_turn
 Board.game
